a238a2f2859778d2a80c30e1e2b9b826179716fa
flavi1 <flavien.guillon@gmail.com>
2023-08-02 19:23:39 +0200

    Create EventDispatcher.php

---
+++ src/PsrSi/EventDispatcher/EventDispatcher.php
@@ -0,0 +1,10 @@
+namespace Psr\EventDispatcher;
+
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Psr\EventDispatcher\ListenerProviderInterface;
+use Psr\EventDispatcher\StoppableEventInterface;
+
+class EventDispatcher implements EventDispatcherInterface
+{
+
+}
