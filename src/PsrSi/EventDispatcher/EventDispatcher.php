d02be9b8c6584cde309d257cc775d23de61db85c
flavi1 <flavien.guillon@gmail.com>
2023-08-02 19:45:55 +0200

    Update EventDispatcher.php

--- src/PsrSi/EventDispatcher/EventDispatcher.php
+++ src/PsrSi/EventDispatcher/EventDispatcher.php
@@ -6,5 +6,17 @@ use Psr\EventDispatcher\StoppableEventInterface;
 
 class EventDispatcher implements EventDispatcherInterface
 {
-
+    /**
+     * Provide all relevant listeners with an event to process.
+     *
+     * @param object $event
+     *   The object to process.
+     *
+     * @return object
+     *   The Event that was passed, now modified by listeners.
+     */
+    public function dispatch(object $event)
+    {
+        
+    }
 }
